fa0ce38ecaf8a6494df644ea381de5b3d7df8dec
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 22:22:36 +0200

    Add project files.

---
+++ Ex_w_2_l_7_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace W_2_l_7_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Przyjęcie na studia");
+
+            Console.WriteLine("Podaj wynik z matematyki");
+            int mat = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj wynik z fizyki");
+            int fiz = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj wynik z chemii");
+            int che = int.Parse(Console.ReadLine());
+
+            if (mat > 70 && fiz>55 && che>45 || (mat +fiz+ che > 150 ) || mat + che > 150 ||mat + fiz>150) {
+                Console.WriteLine("Zostałeś przyjęty");
+            }
+            else { Console.WriteLine("Nie zostałes przyjęty"); };
+
+        }
+    }
+}
